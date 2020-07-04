0394810c98fe4752fab1ac071e1829297c49409e
fajri khairullah <fajri.khairullah@gmail.com>
2020-07-04 08:29:42 +0700

    Started

---
+++ restservice/src/main/java/com/bri/restservice/configs/ApplicationProperties.java
@@ -0,0 +1,16 @@
+package com.bri.restservice.configs;
+
+public class ApplicationProperties {
+    
+    private String dataDir;
+
+    public String getDataDir() {
+        return dataDir;
+    }
+
+    public void setDataDir(String dataDir) {
+        this.dataDir = dataDir;
+    }
+        
+    
+}
